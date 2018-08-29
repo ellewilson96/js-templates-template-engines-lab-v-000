7fefc1a64e269a1c9f00a35ea585727dff1fac2f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-29 15:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
-function postComment() {
-    //create template function
-    const commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
-     // grab raw data
-    const commenter = document.getElementById("commenterName").value;
-    const comment = document.getElementById("commentBody").value;
+function createPost() {
 
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-    // Comments Section to attach to
-    const commentsDiv = document.getElementById("comments");
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+   var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
 
-    //execute template function with JSON object for the interpolated values
-    var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
-
-    //append rather than replace!
-    commentsDiv.innerHTML += templateHTML;
-  }
-   //submitting the form should create the page template with the post inside of it
-  function createPost(){
-
-    //create template functions (page, post, comments)
-    const pageTemplate = _.template(document.getElementById("page-template").innerHTML)
-    const postTemplate = _.template(document.getElementById("post-template").innerHTML)
-    const commentsTemplate = _.template(document.getElementById("comments-template").innerHTML)
-     // grab raw data
-    // post raw data ONLY
-    const title = document.getElementById("postTitle").value
-    const author = document.getElementById("postAuthor").value
-    const body = document.getElementById("postBody").value
-
-    // Define Section to Append HTML to
-    //post appendage
-    const pageSection = document.getElementsByTagName("main")[0]
-    const postSection = document.getElementById("post")
-    const commentSection = document.getElementsByTagName("footer")[0].innerHTML
-     //execute all template functions
-    const pageHTML = pageTemplate()
-    const postHTML = postTemplate({'title': title, "author": author, "body": body})
-    const commentsHTML = commentsTemplate()
-     //append rather than replace!
-    pageSection.innerHTML += pageHTML;
-    postSection.innerHTML += postHTML;
-    commentSection.innerHTML += commentsHTML;
-
-  }
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
+   postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+ function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+   var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+   var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+} 
